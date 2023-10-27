480a5b612413c3f1b2c89c41c9dca4e10005f203
aninhaaa <110306285+gohktsjuju@users.noreply.github.com>
2023-10-27 08:55:41 -0300

    .

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-document.querySelector('.tecla_pom');
+function TocaSomPom{
+document.querySelector('#som_tecla_pom').play('');
+}
+document.querySelectorAll(.'tecla')
+const listadeteclas = document.querySelectorAll(.tecla);
+listadeteclas[0].onclik = TocaSomPom;
+
+listadeteclas[1].onclik = tocasomclap;
+listadeteclas[2].onclik = tocasomtim;
+listadeteclas[3].onclik = tocasompuff;
+
+
+
+
+
